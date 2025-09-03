d4b80313b7384ff9753647c5c4759df89ccb87e8
EdwinAE212 <113155807+EdwinAE212@users.noreply.github.com>
2025-09-03 15:20:26 -0700

    Creacion de la estructura del proyecto, base de la conexion aun no probada y cracion de los modelos

---
+++ conexion.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const conectarbd = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("Conexion exitosa a mongodb");
+    } catch (error) {
+        console.error("Error de conexion a mongodb", error);
+        process.exit(1);
+    }
+};
+
+module.exports = conectarbd;
